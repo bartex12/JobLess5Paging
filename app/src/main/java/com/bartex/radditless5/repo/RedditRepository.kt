2557542b8801ec4f471c2a2c8d14bf922ebd6188
bartex12 <37575336+bartex12@users.noreply.github.com>
2021-11-24 20:36:38 +0300

    начало - сделал на Paging2 без RxJava

--- app/src/main/java/com/bartex/radditless5/repo/RedditRepository.kt
+++ app/src/main/java/com/bartex/radditless5/repo/RedditRepository.kt
@@ -1,12 +1,15 @@
 package com.bartex.radditless5.repo
 
+import android.util.Log
 import com.bartex.radditless5.entity.Children
 import com.bartex.radditless5.entity.ResultFromNet
 import retrofit2.Response
 import java.util.concurrent.CountDownLatch
 
 class RedditRepository: IRedditRepository {
-
+    companion object{
+        const val TAG = "33333"
+    }
     override fun loadData(pageNumber: Int, pageSize: Int): List<Children>? {
        var redditList : List<Children>? = listOf()
         var response: Response<ResultFromNet?>?
@@ -16,6 +19,8 @@ class RedditRepository: IRedditRepository {
             val iphoto: IReddit = RedditSource.API
             response = iphoto.loadRedditData().execute()
             val apiResults:ResultFromNet?  =  response?.body()
+            val dist = apiResults?.data?.dist
+            Log.d(TAG, "loadData: dist = $dist")
             //переходим к списку Children
             redditList = apiResults?.data?.children
             //разрешаем работу основного потока для возврата данных после окончания этого
